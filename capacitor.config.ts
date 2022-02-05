c20a870501c064e60c9ec29e44336fefe9b5de82
nshejteri <nshejteri@gmail.com>
2022-02-05 14:27:58 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter.vue',
+  appName: 'tabsTestVue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,66 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Tab 1</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Tab 1</ion-title>
+                </ion-toolbar>
+            </ion-header>
+
+            <div v-for="number in [1,2,3,4,5,6,7,8,9,10]" :key="number">
+                <ion-card button>
+                    <ion-item>
+                        <ion-label>{{number}}</ion-label>
+                    </ion-item>
+
+                    <ion-card-content>
+                        This is content, without any paragraph or header tags,
+                        within an ion-card-content element.
+                    </ion-card-content>
+                </ion-card>
+            </div>
+<!--            <ExploreContainer name="Tab 1 page"/>-->
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    onIonViewDidEnter,
+    onIonViewWillEnter
+} from '@ionic/vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default {
+    components: { IonLabel, IonItem, IonCard, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+    setup () {
+        let will = 0;
+        let did = 0;
+
+        onIonViewDidEnter(() => {
+            did = Date.now();
+            console.log('TAB 1 page did enter', did);
+            console.log('DIFF TAB 1 ', did - will);
+        });
+
+        onIonViewWillEnter(() => {
+            will = Date.now();
+            console.log('TAB 1 page will enter', will);
+        });
+    }
+};
+</script>
